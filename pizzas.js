e9c769565424ce8db211fef13f7551fbf618e7e1
anderson-zip <andersonniki@Hotmail.com>
2025-06-28 01:03:58 -0300

    ajustes2

--- pizzas.js
+++ pizzas.js
@@ -1,7 +1,7 @@
 let pizzaJson = [
-    {id:1, name:'Brahma Duplo Malte', img:'images/brahmaDP.png', price:20.19, sizes:['100g', '530g', '860g'], description:''},
-    {id:2, name:'Heineken', img:'images/heineken.png', price:18.00, sizes:['320g', '530g', '860g'], description:''},
-    {id:3, name:'Heineken Zero', img:'images/heineken_zero.png', price:17.45, sizes:['320g', '530g', '860g'], description:''},
-    {id:4, name:'Itaipava', img:'images/itaipava.png', price:19.77, sizes:['320g', '530g', '860g'], description:''},
-    {id:5, name:'Budweiser', img:'images/Budweiser.png', price:21.43, sizes:['320g', '530g', '860g'], description:''}
+    {id:1, name:'Brahma Duplo Malte', img:'images/brahmaDP.png', price:20.19, sizes:['', '', ''], description:''},
+    {id:2, name:'Heineken', img:'images/heineken.png', price:18.00, sizes:['', '', ''], description:''},
+    {id:3, name:'Heineken Zero', img:'images/heineken_zero.png', price:17.45, sizes:['', '', ''], description:''},
+    {id:4, name:'Itaipava', img:'images/itaipava.png', price:19.77, sizes:['', '', ''], description:''},
+    {id:5, name:'Budweiser', img:'images/Budweiser.png', price:21.43, sizes:['', '', ''], description:''}
 ];
